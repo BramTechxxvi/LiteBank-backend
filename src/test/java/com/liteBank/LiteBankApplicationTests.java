a13dbb282f3f8017318f0fba5d7c711a1d0b02ce
BramTechxxvi <bramtechxxvi@gmail.com>
2025-08-07 10:12:45 +0100

    new

---
+++ src/test/java/com/liteBank/LiteBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.liteBank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiteBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
