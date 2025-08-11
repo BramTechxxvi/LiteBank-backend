5f7192a8d3fc13605de1bf293018fe902e08ef53
BramTechxxvi <bramtechxxvi@gmail.com>
2025-08-11 10:54:58 +0100

    new

---
+++ src/main/java/com/liteBank/dtos/DepositRequest.java
@@ -0,0 +1,4 @@
+package com.liteBank.dtos;
+
+public class DepositRequest {
+}
--- src/test/java/com/liteBank/service/AccountServiceTest.java
+++ src/test/java/com/liteBank/service/AccountServiceTest.java
@@ -1,4 +1,15 @@
 package com.liteBank.service;
 
+import com.liteBank.dtos.DepositRequest;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
 public class AccountServiceTest {
+
+    @Test
+    void testCanDeposit() {
+        DepositRequest depositRequest = new DepositRequest();
+    }
+
 }
