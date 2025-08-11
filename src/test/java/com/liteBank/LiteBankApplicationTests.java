32fd48cd22a26a34d39c1c3a84069e123b112eb1
BramTechxxvi <bramtechxxvi@gmail.com>
2025-08-11 10:49:50 +0100

    new

--- src/test/java/com/liteBank/LiteBankApplicationTests.java
+++ src/test/java/com/liteBank/LiteBankApplicationTests.java
@@ -1,5 +1,6 @@
 package com.liteBank;
 
+import com.zaxxer.hikari.HikariDataSource;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -12,8 +13,9 @@ class LiteBankApplicationTests {
 
 	@Test
 	void testCanConnectToDataBase() {
-		try(HikariDataSource) {
-
+		try(HikariDataSource hikariDaraSource = new HikariDataSource()) {
+			hikariDaraSource.setJdbcUrl("jdbc:postgresql://localhost:5432/postgres");
+			hikariDaraSource.setUsername();
 		} catch() {
 
 		}
