b13eb2d40809a5a2f31b80e438b33cf087a5814c
BramTechxxvi <bramtechxxvi@gmail.com>
2025-08-11 15:24:18 +0100

    new

--- src/main/java/com/liteBank/service/TransactionService.java
+++ src/main/java/com/liteBank/service/TransactionService.java
@@ -1,4 +1,9 @@
 package com.liteBank.service;
 
+import com.liteBank.dtos.request.CreateTransactionRequest;
+import com.liteBank.dtos.response.CreateTransactionResponse;
+
 public interface TransactionService {
+
+    CreateTransactionResponse create(CreateTransactionRequest request);
 }
