757e924a9dbec09affbd9a7b7bc7fefc348f26a0
BramTechxxvi <bramtechxxvi@gmail.com>
2025-08-11 11:13:30 +0100

    new

--- src/main/java/com/liteBank/service/AccountService.java
+++ src/main/java/com/liteBank/service/AccountService.java
@@ -1,6 +1,9 @@
 package com.liteBank.service;
 
+import com.liteBank.dtos.request.DepositRequest;
+import com.liteBank.dtos.response.DepositResponse;
+
 public interface AccountService {
 
-    De
+    DepositResponse deposit(DepositRequest depositRequest);
 }
