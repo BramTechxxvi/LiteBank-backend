700c413b27b07bbb8eba33c43db53d628490e702
BramTechxxvi <bramtechxxvi@gmail.com>
2025-08-11 10:57:58 +0100

    new

--- src/main/java/com/liteBank/dtos/DepositRequest.java
+++ src/main/java/com/liteBank/dtos/DepositRequest.java
@@ -1,7 +1,12 @@
 package com.liteBank.dtos;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.math.BigDecimal;
 
+@Setter
+@Getter
 public class DepositRequest {
 
     private String senderAccountNumber;
