b0843d3cca726631dd65cef11f12da19c32bba42
BramTechxxvi <bramtechxxvi@gmail.com>
2025-08-11 11:10:34 +0100

    new

--- src/main/java/com/liteBank/dtos/DepositRequest.java
+++ src/main/java/com/liteBank/dtos/DepositRequest.java
@@ -1,12 +1,16 @@
 package com.liteBank.dtos;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
 import java.math.BigDecimal;
 
 @Setter
 @Getter
+@RequiredArgsConstructor
+@AllArgsConstructor
 public class DepositRequest {
 
     private String senderAccountNumber;
--- src/test/java/com/liteBank/service/AccountServiceTest.java
+++ src/test/java/com/liteBank/service/AccountServiceTest.java
@@ -1,15 +1,24 @@
 package com.liteBank.service;
 
 import com.liteBank.dtos.DepositRequest;
+import com.liteBank.dtos.PaymentMethod;
+import com.liteBank.dtos.response.DepositResponse;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.math.BigDecimal;
 
 @SpringBootTest
 public class AccountServiceTest {
 
     @Test
     void testCanDeposit() {
-        DepositRequest depositRequest = new DepositRequest();
+        DepositRequest depositRequest = new DepositRequest(
+                "0801234567",
+                "0701234567",
+                new BigDecimal(10_000),
+                PaymentMethod.CARD);
     }
+    DepositResponse depositResponse = accountService.deposit(depositRequest);
 
 }
