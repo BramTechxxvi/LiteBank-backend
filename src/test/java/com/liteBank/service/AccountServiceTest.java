b9a0568529463619324b6c7615429d16fe070fca
BramTechxxvi <bramtechxxvi@gmail.com>
2025-08-11 17:02:19 +0100

    new

--- src/test/java/com/liteBank/service/AccountServiceTest.java
+++ src/test/java/com/liteBank/service/AccountServiceTest.java
@@ -1,5 +1,6 @@
 package com.liteBank.service;
 
+import com.liteBank.dtos.enums.TransactionStatus;
 import com.liteBank.dtos.request.DepositRequest;
 import com.liteBank.dtos.enums.PaymentMethod;
 import com.liteBank.dtos.response.DepositResponse;
@@ -22,11 +23,12 @@ public class AccountServiceTest {
     @Sql(scripts = "/db/data.sql")
     void testCanDeposit() {
         DepositRequest depositRequest = new DepositRequest();
-        depositRequest.setAccountNumber("0701234567");
+        depositRequest.setAccountNumber("0123456789");
         depositRequest.setPaymentMethod(PaymentMethod.CARD);
         depositRequest.setAmount(new BigDecimal(10_000));
 
         DepositResponse depositResponse = accountService.deposit(depositRequest);
-        assertNotNull(depositResponse.getTransactionId());
+        assertNotNull(depositResponse);
+        assertEquals(TransactionStatus.SUCCESS, depositResponse.getTransactionStatus());
     }
 }
