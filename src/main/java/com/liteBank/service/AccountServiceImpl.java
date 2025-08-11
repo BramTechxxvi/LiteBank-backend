57f49d1425e4dcfe964513cb6e88968d2af74d9c
BramTechxxvi <bramtechxxvi@gmail.com>
2025-08-11 11:24:32 +0100

    new

--- src/main/java/com/liteBank/service/AccountServiceImpl.java
+++ src/main/java/com/liteBank/service/AccountServiceImpl.java
@@ -1,4 +1,12 @@
 package com.liteBank.service;
 
-public class AccountServiceImpl {
+import com.liteBank.dtos.request.DepositRequest;
+import com.liteBank.dtos.response.DepositResponse;
+
+public class AccountServiceImpl implements AccountService {
+
+    @Override
+    public DepositResponse deposit(DepositRequest depositRequest) {
+        return null;
+    }
 }
