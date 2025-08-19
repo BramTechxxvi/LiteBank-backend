d968e82e5fc27f75f9c6d394dcecc81f1f438089
BramTechxxvi <bramtechxxvi@gmail.com>
2025-08-19 10:59:31 +0100

    new

--- src/main/java/com/liteBank/dtos/enums/PaymentMethod.java
+++ src/main/java/com/liteBank/dtos/enums/PaymentMethod.java
@@ -2,6 +2,15 @@ package com.liteBank.dtos.enums;
 
 public enum PaymentMethod {
     CARD,
-    TRANSFER,
-    CASH
+    BANK_TRANSFER,
+    CASH,
+    MOBILE_MONEY,
+    E_WALLET,
+    CHEQUE,
+    CRYPTO,
+    ONLINE_BANKING,
+    POS,
+    REVERSAL,
+    DEPOSIT,
+    WITHDRAWAL
 }
