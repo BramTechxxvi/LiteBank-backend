221a3d2734b6a60b293e1f341b778d0bdd8b490a
BramTechxxvi <bramtechxxvi@gmail.com>
2025-08-19 10:14:44 +0100

    new

--- src/main/java/com/liteBank/config/BeanConfig.java
+++ src/main/java/com/liteBank/config/BeanConfig.java
@@ -1,11 +1,16 @@
 package com.liteBank.config;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class BeanConfig {
 
     @Bean
     public BeanConfig modelMapper() {
+        ModelMapper modelMapper = new ModelMapper();
+        modelMapper.getConfiguration().setFieldMatchingEnabled(true);
         return new BeanConfig();
     }
 }
