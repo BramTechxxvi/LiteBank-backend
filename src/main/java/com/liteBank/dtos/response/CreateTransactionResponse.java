ba72c64943af7266ee2d3e2443baebefa7f5cbe5
BramTechxxvi <bramtechxxvi@gmail.com>
2025-08-19 11:02:02 +0100

    new

--- src/main/java/com/liteBank/dtos/response/CreateTransactionResponse.java
+++ src/main/java/com/liteBank/dtos/response/CreateTransactionResponse.java
@@ -1,11 +1,13 @@
 package com.liteBank.dtos.response;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
 @Setter
 @Getter
+@AllArgsConstructor
 @RequiredArgsConstructor
 public class CreateTransactionResponse {
     private String id;
--- src/main/java/com/liteBank/dtos/response/TransactionResponse.java
+++ src/main/java/com/liteBank/dtos/response/TransactionResponse.java
@@ -3,9 +3,11 @@ package com.liteBank.dtos.response;
 import jakarta.persistence.Id;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 @Setter
 @Getter
+@ToString
 public class TransactionResponse {
     @Id
     private String id;
