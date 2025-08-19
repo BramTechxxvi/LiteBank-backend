8cfd28f28422547c5b9ea4c0ad08f49d452e7df9
BramTechxxvi <bramtechxxvi@gmail.com>
2025-08-19 11:02:40 +0100

    new

--- src/main/java/com/liteBank/dtos/response/ViewAccountResponse.java
+++ src/main/java/com/liteBank/dtos/response/ViewAccountResponse.java
@@ -1,11 +1,14 @@
 package com.liteBank.dtos.response;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
 @Setter
 @Getter
+@AllArgsConstructor
 @RequiredArgsConstructor
 public class ViewAccountResponse {
+    private String balance;
 }
