3f2be3292d0fc01372434713fa6e84a184cd6079
BramTechxxvi <bramtechxxvi@gmail.com>
2025-08-19 12:14:15 +0100

    new

--- src/main/java/com/liteBank/data/repository/TransactionRepository.java
+++ src/main/java/com/liteBank/data/repository/TransactionRepository.java
@@ -2,6 +2,7 @@ package com.liteBank.data.repository;
 
 import com.liteBank.data.models.Transaction;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -10,5 +11,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface TransactionRepository extends JpaRepository<Transaction, String> {
     @Query("select t from Transaction t where t.accountNumber=:accountNumber")
-    Page<Transaction> getByAccountNumber(String accountNumber);
+    Page<Transaction> retrieveTransactionsByAccountNumber(String accountNumber, Pageable pageable);
 }
