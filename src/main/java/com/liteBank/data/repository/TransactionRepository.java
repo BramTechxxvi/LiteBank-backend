cd6e6fdad8b2dd56c1068bfc7937364260877645
BramTechxxvi <bramtechxxvi@gmail.com>
2025-08-19 10:35:08 +0100

    new

--- src/main/java/com/liteBank/data/repository/TransactionRepository.java
+++ src/main/java/com/liteBank/data/repository/TransactionRepository.java
@@ -9,5 +9,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface TransactionRepository extends JpaRepository<Transaction, String> {
-    Page<Transaction> retrieveByAccountNumber(String accountNumber, Pageable pageable);
+    Page<Transaction> readByAccountNumber(String accountNumber, Pageable pageable);
 }
--- src/test/java/com/liteBank/data/repository/TransactionRepositoryTest.java
+++ src/test/java/com/liteBank/data/repository/TransactionRepositoryTest.java
@@ -1,8 +1,10 @@
 package com.liteBank.data.repository;
 
+import com.liteBank.data.models.Transaction;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 
@@ -18,7 +20,9 @@ class TransactionRepositoryTest {
     void retrieveAccountNumberTest() {
         String accountNumber = "0123456789";
         Pageable pageable = PageRequest.of(0, 5);
-        transactionRepository.re
+        Page<Transaction> transactions = transactionRepository.retrieveByAccountNumber(accountNumber, pageable);
+        assertThat(transactions).isNotNull();
+        assertThat(transactions.getContent().size().isEqualto(5));
     }
 
 }
