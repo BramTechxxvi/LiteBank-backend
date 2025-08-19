b175d2dd63dd38349a7a6d6a125d8cfb9455ab6c
BramTechxxvi <bramtechxxvi@gmail.com>
2025-08-19 10:23:29 +0100

    new

--- src/main/java/com/liteBank/data/repository/TransactionRepository.java
+++ src/main/java/com/liteBank/data/repository/TransactionRepository.java
@@ -1,8 +1,11 @@
 package com.liteBank.data.repository;
 
 import com.liteBank.data.models.Transaction;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
 
 @Repository
 public interface TransactionRepository extends JpaRepository<Transaction, String> {
---
+++ src/test/java/com/liteBank/data/repository/TransactionRepositoryTest.java
@@ -0,0 +1,7 @@
+package com.liteBank.data.repository;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TransactionRepositoryTest {
+
+}
