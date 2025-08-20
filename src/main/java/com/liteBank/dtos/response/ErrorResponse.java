d126f9d5a0d8391659908462a1215e9d2831d8e5
BramTechxxvi <bramtechxxvi@gmail.com>
2025-08-20 15:53:38 +0100

    new

--- src/main/java/com/liteBank/dtos/response/ErrorResponse.java
+++ src/main/java/com/liteBank/dtos/response/ErrorResponse.java
@@ -1,6 +1,9 @@
 package com.liteBank.dtos.response;
 
 public class ErrorResponse(T) {
+    private T response;
 
-    public ErrorResponse()
+    public ErrorResponse(T response) {
+        this.response = response;
+    }
 }
